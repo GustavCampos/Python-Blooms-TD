abfc5b2adf1bbc35b10a788326878c24acf51b73
gustavcampos <gustavop.campos2004@gmail.com>
2023-05-26 16:46:35 -0300

    resolution changes

--- main.py
+++ main.py
@@ -21,21 +21,23 @@ def main():
     # loading custom icon_________________________________________________________________
     game_icon = pygame.image.load(os.path.join(current_path, 'data', 'imgs', 'icon.png'))
     pygame.display.set_icon(game_icon)
-
-    # loading test map for color picker feature___________________________________________
-    color_picker_tester = pygame.image.load(os.path.join(current_path, 'data', 'imgs', 'color_picker_tester.png'))
     
     display_surface = pygame.display.set_mode(
         [int(config_opt["DISPLAY"]["SCREEN_WIDTH"]), int(config_opt["DISPLAY"]["SCREEN_HEIGHT"])],
         vsync=int(config_opt["DISPLAY"]["VSYNC_OPTION"])
     )
     
-    surface_map = pygame.Surface((display_surface.get_height(), display_surface.get_height()))
-
+    surface_map = pygame.Surface((
+        display_surface.get_height() * (4/3), 
+        display_surface.get_height()
+    ))
+    
+    # loading test map for color picker feature___________________________________________
+    color_picker_tester = pygame.image.load(os.path.join(current_path, 'data', 'imgs', 'map_test.png'))
 
     
-    map_track = parser.get_waypoints_list(
-        os.path.join(current_path, 'data', 'config', 'map_config', 'map1.txt')
+    map_track, rp_list = parser.get_waypoints_list(
+        os.path.join(current_path, 'data', 'config', 'map_config', 'map1.txt'), surface_map
     )
     blooms_track = parser.get_wave_list_from_file(
         os.path.join(current_path, 'data', 'config', 'wave_config', 'easy.txt')
@@ -50,17 +52,12 @@ def main():
         dt *= 60
         last_time = time.time()
 
-        
+        #Reset Screen Image_______________________________________
         display_surface.fill((0, 0, 255))
         surface_map.fill((0,0,0))
         surface_map.blit(color_picker_tester, (0,0))
-        display_surface.blit(surface_map, (0,0))
+        #_________________________________________________________
         
-        # #Debug UI________________________________
-        for item in map_track:
-            item.draw(display_surface)
-        # bloom_factory.draw(surface)
-        #________________________________________
                 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -68,8 +65,12 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                  if event.button == 1:  # Verifica se o botão esquerdo do mouse foi pressionado
                     mouse_x, mouse_y = event.pos
-                    pixel_color = display_surface.get_at((mouse_x, mouse_y)) # identifica a cor do pixel
-                    print("Posição do clique: x =", mouse_x, "y =", mouse_y)
+                    
+                    mouse_x_p = (mouse_x * 100) / surface_map.get_width()
+                    mouse_y_p = (mouse_y * 100) / surface_map.get_height()
+                    
+                    pixel_color = surface_map.get_at((mouse_x, mouse_y)) # identifica a cor do pixel
+                    print("Posição do clique: x =", mouse_x_p, "y =", mouse_y_p)
                     print("Cor do pixel:", pixel_color)
 
             if event.type == pygame.KEYDOWN:
@@ -81,12 +82,25 @@ def main():
                     config_opt["max_fps"] = 90
                 if event.key == pygame.K_4:
                     config_opt["max_fps"] = 0
-            
-        bloom_factory.run_map(display_surface, dt)
         
-        pygame.display.set_caption(f"OpenBloons : {pygame_clock.get_fps()}")
+        #Screen Update__________________________________________________________    
+        bloom_factory.run_map(surface_map, dt)
+        display_surface.blit(surface_map, (0,0))
+        
+        # #Debug UI________________________________
+        for item in map_track:
+            item.draw(display_surface)
+        for item in rp_list:
+            item.draw(display_surface)
+        # bloom_factory.draw(surface)
+        #________________________________________
+        
+        
         pygame.display.update()
+        #_______________________________________________________________________
+        
         pygame_clock.tick(int(config_opt["DISPLAY"]["MAX_FPS"]))
+        pygame.display.set_caption(f"OpenBloons : {pygame_clock.get_fps()}")
                 
     pygame.font.quit()
     pygame.display.quit()
